ccdf8e970024e56414ff256c07b3aef71a640227
John Macka <macka2004@gmail.com>
2024-02-22 16:20:06 +1000

    Revert "libcameraservice: Add support to set vendor tag with client package name"
    This reverts commit 4a764a52ae04a849a16b350b5de05a8aa0fbcd23.

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -152,9 +152,6 @@ const String16 CameraService::kWatchAllClientsFlag("all");
 // Set to keep track of logged service error events.
 static std::set<String8> sServiceErrorEventSet;
 
-// Current camera package name
-static std::string sCurrPackageName;
-
 CameraService::CameraService(
         std::shared_ptr<CameraServiceProxyWrapper> cameraServiceProxyWrapper) :
         mCameraServiceProxyWrapper(cameraServiceProxyWrapper == nullptr ?
@@ -1202,10 +1199,6 @@ Status CameraService::filterGetInfoErrorCode(status_t err) {
     }
 }
 
-std::string CameraService::getCurrPackageName() {
-    return sCurrPackageName;
-}
-
 Status CameraService::makeClient(const sp<CameraService>& cameraService,
         const sp<IInterface>& cameraCb, const String16& packageName, bool systemNativeClient,
         const std::optional<String16>& featureId,  const String8& cameraId,
@@ -2144,8 +2137,6 @@ Status CameraService::connectHelper(const sp<CALLBACK>& cameraCb, const String8&
     ALOGI("CameraService::connect call (PID %d \"%s\", camera ID %s) and "
             "Camera API version %d", packagePid, clientName8.string(), cameraId.string(),
             static_cast<int>(effectiveApiLevel));
-
-    sCurrPackageName = clientName8.string();
 
     nsecs_t openTimeNs = systemTime();
 
--- services/camera/libcameraservice/CameraService.h
+++ services/camera/libcameraservice/CameraService.h
@@ -290,8 +290,6 @@ public:
     // Shared utilities
     static binder::Status filterGetInfoErrorCode(status_t err);
 
-    static std::string getCurrPackageName();
-
     /////////////////////////////////////////////////////////////////////
     // CameraClient functionality
 
